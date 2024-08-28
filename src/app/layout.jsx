ea954958caac8e082f1a8b3498d3a11ebc41fb0d
Himanshu Verma <himanshuverma544@gmail.com>
2024-08-28 10:31:46 +0530

    rn: renamed the files extension from .js to .jsx

---
+++ src/app/layout.jsx
@@ -0,0 +1,27 @@
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+import Header from "@/components/pages/layout/Header";
+import Footer from "@/components/pages/layout/Footer";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Jiaara Jewellery",
+  description: "Ecommerce site for buying Jewellery Items",
+};
+
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <Header/>
+        <main>
+          {children}
+        </main>
+        <Footer/>
+      </body>
+    </html>
+  );
+}
