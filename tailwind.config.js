fbcad1774cef0a3fa316e298a1459164b1e9c8d2
Himanshu Verma <himanshuverma544@gmail.com>
2024-08-29 21:24:26 +0530

    upd: added colors as per the theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,14 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        primaryBackground: "#f5eee4",
+        secondaryBackground: "#3a3039",
+        primaryFont: "#663c59",
+        primaryButton: '#ffddf4',
+        primaryOverlay: "#f7d9ee",
+        primaryFlareEffect: "#663c59"
+      },
     },
   },
   plugins: [],
