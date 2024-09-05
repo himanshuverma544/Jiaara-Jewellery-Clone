e19ddea412083ed7c2d81c3ea4a40d19fb009e1a
Himanshu Verma <himanshuverma544@gmail.com>
2024-09-05 10:34:50 +0530

    add: created the NavItem component

---
+++ src/components/general/NavItem.jsx
@@ -0,0 +1,45 @@
+import Link from 'next/link';
+
+import Icon from "./Icon";
+
+import useLinkActive from "../../utils/hooks/general/useLinkActive";
+
+
+export default function NavItem({
+  title = "",
+  href = "https://www.example.com",
+  icon = {
+    general: <></>,
+    active: <></>,
+    inactive: <></>
+  },
+  target = "_self",
+  linkClass = "",
+  iconClass = "",
+  titleClass = ""
+}) {
+
+  const isActive = useLinkActive({ href });
+
+  return (
+    <li className="nav-item">
+      <Link
+        className={`${linkClass}`}
+        href={href}
+        target={target}
+      >
+        {(icon.general || icon.active || icon.inactive) &&
+          <Icon
+            className={`${iconClass}`}
+            icon={isActive ? icon?.active : icon?.inactive ?? icon?.general}
+          />
+        }
+        {title &&
+          <div className={`${titleClass}`}>
+            {title}
+          </div>
+        }
+      </Link>
+    </li>  
+  );
+}
