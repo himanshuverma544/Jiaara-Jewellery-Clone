16f40872010eb959dd85c4029a1644ed5a49238c
Himanshu Verma <himanshuverma544@gmail.com>
2024-09-05 10:31:27 +0530

    upd: updates the routes objects

--- src/routes.js
+++ src/routes.js
@@ -1,31 +1,57 @@
+import { GoHome } from "react-icons/go";
+import { GoHomeFill } from "react-icons/go";
+
+import { IoDiamondOutline } from "react-icons/io5";
+import { IoDiamond } from "react-icons/io5";
+
+import { IoInformationOutline } from "react-icons/io5";
+import { IoInformationSharp } from "react-icons/io5";
+
+import { MdOutlineLocalPhone } from "react-icons/md";
+import { MdLocalPhone } from "react-icons/md";
+
+import { IoPricetagsOutline } from "react-icons/io5";
+import { IoPricetagsSharp } from "react-icons/io5";
+
+
 export const HOME = {
   id: 1,
   title: "Home",
-  pathName: "/"
+  pathname: "/",
+  inactiveIcon: GoHome,
+  activeIcon: GoHomeFill
 }
 
 export const JEWELLERY = {
   id: 2,
   title: "Jewellery",
-  pathName: "#"
+  pathname: "#",
+  inactiveIcon: IoDiamondOutline,
+  activeIcon: IoDiamond
 }
 
 export const ABOUT = {
   id: 3,
   title: "About",
-  pathName: "#"
+  pathname: "#",
+  inactiveIcon: IoInformationOutline,
+  activeIcon: IoInformationSharp
 }
 
 export const CONTACT = {
   id: 4,
   title: "Contact",
-  pathName: "#"
+  pathname: "#",
+  inactiveIcon: MdOutlineLocalPhone,
+  activeIcon: MdLocalPhone
 }
 
 export const SALE = {
   id: 5,
   title: "Sale",
-  pathName: "#"
+  pathname: "#",
+  inactiveIcon: IoPricetagsOutline,
+  activeIcon: IoPricetagsSharp
 }
 
 
