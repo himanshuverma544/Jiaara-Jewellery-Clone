8b6405bf49007d423024a0822797fade525f957a
Himanshu Verma <himanshuverma544@gmail.com>
2024-09-09 10:35:17 +0530

    upd: tried to suppress the pure-react-carousel and Image component warnings

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,20 @@
 /** @type {import('next').NextConfig} */
 
-const nextConfig = {};
+const nextConfig = {
+
+  webpack(config, { dev }) {
+    if (dev) {
+      config.ignoreWarnings = [
+        {
+          module: /react-carousel\.es\.css\.map$/,
+        },
+        {
+          message: /Image with src .* has "fill" and parent element with invalid "position"/,
+        }
+      ];
+    }
+    return config;
+  }
+};
 
 export default nextConfig;
