50743017dcf1457f7bfa6d7d98cc75ca7b374cae
Himanshu Verma <himanshuverma544@gmail.com>
2024-09-09 10:40:24 +0530

    add: added the file and managed the errors system

---
+++ src/context-API/actions/action.creators.js
@@ -0,0 +1,35 @@
+import { STORE_DATA, INVALID_TYPE_OF_DATA } from "./action.types";
+
+
+const storeData = (data, typeOfData) => {
+
+  let actionType = INVALID_TYPE_OF_DATA;
+
+  const dataAllowed = ["states", "nodes", "objects", "arrays", "refVars", "vars"];
+
+  if (typeof data === "object" && typeof typeOfData === "string") {
+    for (let allowedData of dataAllowed) {
+      if (allowedData === typeOfData) {
+        actionType = STORE_DATA;
+        break;
+      }
+    }
+  }
+
+  if ( actionType === INVALID_TYPE_OF_DATA ) {
+    console.error("Invalid Type of Data");
+  }
+
+  return {
+    type: actionType,
+    payload: {
+      typeOfData,
+      data
+    }
+  };
+};
+
+
+export { 
+  storeData
+};
