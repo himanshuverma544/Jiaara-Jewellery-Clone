c23878530c6972f2d4f9ecbd7161b741650f9ecd
Himanshu Verma <himanshuverma544@gmail.com>
2024-09-09 10:43:44 +0530

    upd: updated header as per it's design requirement using context-api

--- src/components/pages/layout/Header.jsx
+++ src/components/pages/layout/Header.jsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React from 'react';
+import { useContext } from 'react';
 
 import Image from 'next/image';
 
@@ -14,6 +14,7 @@ import { IoCartOutline } from "react-icons/io5";
 import Icon from "../../general/Icon";
 import NavItem from '@/components/general/NavItem';
 
+import { context } from "../../../context-API/context";
 
 
 const routes = getAllRoutes();
@@ -25,19 +26,33 @@ const headerBtnsIcon = [
   AiOutlineUser
 ];
 
+const brandLogo = {
+  white: "/assets/logos/jiaara-white.png",
+  black: "/assets/logos/jiaara-black.png"
+};
+
 export default function Header() {
 
+  const { data: { triggered }, data: { states } } = useContext(context);
+  const isHeroSecVisible = triggered && states?.isHeroSecVisible;
+
   return (
-    <header id="header" className="w-full flex items-center fixed px-3 py-1 z-10 bg-transparent md:px-5 lg:px-10">
+    <header
+      id="header"
+      className={`
+        w-full flex items-center fixed px-3 py-1 z-10
+        ${isHeroSecVisible ? "bg-transparent" : "bg-secondaryBackground"}
+        md:px-5 lg:px-10
+      `}
+    >
 
       <div className="app-brand img-cont-wrapper">
         <div className="img-cont size-[100px] relative">
           <Image
-            src="/assets/logos/jiaara-white.png"
+            fill
+            src={isHeroSecVisible ? brandLogo.white : brandLogo.black}
             alt="brand-logo"
-            layout="fill"
-            objectFit="cover"
-            objectPosition="center"
+            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
           />
         </div>
       </div>
@@ -45,7 +60,12 @@ export default function Header() {
       <div className="wrapper w-full flex relative">
         
         <nav className="navbar w-[inherit] fixed bottom-0 left-0 lg:static">
-          <ul className="nav-items-cont flex justify-around items-center px-3 bg-primaryBackground lg:justify-evenly lg:px-0 lg:py-5 lg:uppercase lg:bg-transparent lg:text-white">
+          <ul className={`
+            nav-items-cont
+            flex justify-around items-center px-3
+            bg-secondaryBackground
+            lg:justify-evenly lg:px-0 lg:py-5 lg:uppercase lg:bg-transparent
+          `}>
             {routes.map(route => 
               <NavItem
                 key={route?.id}
@@ -55,16 +75,35 @@ export default function Header() {
                   active: route?.activeIcon,
                   inactive: route?.inactiveIcon
                 }}
-                linkClass="flex flex-col items-center justify-center gap-2 px-3 py-3 rounded text-primaryFont lg:flex-row lg:gap-2 lg:text-white"
-                iconClass="nav-item-icon text-base lg:hidden"
-                titleClass="nav-item-title text-xs"
+                linkClass={`
+                  flex flex-col items-center justify-center gap-2 px-3 py-3
+                  rounded
+                  lg:flex-row lg:gap-2
+                `}
+                iconClass={`
+                  nav-item-icon
+                  text-base
+                  text-primaryFont
+                  lg:hidden
+                  ${isHeroSecVisible ? "lg:text-white" : "lg:text-primaryFont"}
+                `}
+                titleClass={`
+                  nav-item-title
+                  text-xs
+                  text-primaryFont
+                  ${isHeroSecVisible ? "lg:text-white" : "lg:text-primaryFont"}
+                `}
               />
             )}
           </ul>
         </nav>
 
         <div className="nav-icons-cont grow flex flex-col items-end justify-center py-8 lg:items-baseline">
-          <div className="flex gap-4 px-3 text-lg text-white">
+          <div className={`
+            flex gap-4 px-3
+            text-lg
+            ${isHeroSecVisible ? "text-white" : "text-primaryFont"}
+          `}>
             {headerBtnsIcon.map((icon, index) => (
               <button key={index}>
                 <Icon icon={icon}/>
@@ -73,7 +112,11 @@ export default function Header() {
           </div>
         </div>
 
-        <hr className="w-full absolute top-[93%] right-0"/>
+        <hr className={`
+          design-line
+          w-full absolute top-[93%] right-0
+          ${isHeroSecVisible ? "border-white" : "border-primaryFont"}
+        `}/>
 
       </div>
      
