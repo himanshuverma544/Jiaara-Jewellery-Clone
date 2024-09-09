e490f69295db2761ecccfa1fa5455413d5f06482
Himanshu Verma <himanshuverma544@gmail.com>
2024-09-09 10:44:46 +0530

    upd: gave some gap between the sections of homepage

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,15 @@
+import Hero from "@/components/pages/home/Hero";
+import Categories from "@/components/pages/home/Categories";
+import FeaturedProducts from "@/components/pages/home/featured-products/FeaturedProducts";
+
+
 export default function Home() {
 
   return (
-    <>
-    </>
+    <div className="homepage flex flex-col gap-12">
+      <Hero/>
+      <Categories/>
+      <FeaturedProducts/>
+    </div>
   );
 }
