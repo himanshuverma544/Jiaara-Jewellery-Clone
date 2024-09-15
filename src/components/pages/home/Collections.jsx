0863f57f1de028a79a71aac4cfa6afb168c014f2
Himanshu Verma <himanshuverma544@gmail.com>
2024-09-15 20:47:55 +0530

    add: created the collections section

---
+++ src/components/pages/home/Collections.jsx
@@ -0,0 +1,250 @@
+'use client';
+
+import React from "react";
+
+import Image from "next/image";
+import useWindowSize from "@/utils/hooks/general/useWindowSize";
+
+const collections = [
+  {
+    id: 1,
+    label: '18K Gold',
+    image: '/assets/pages/homepage/sections/7-Collection/2.jpg'
+  },
+  { 
+    id: 2,
+    label: 'Brass',
+    image: '/assets/pages/homepage/sections/7-Collection/1.jpg'
+  },
+  {
+    id: 3,
+    label: 'For Him',
+    image: '/assets/pages/homepage/sections/7-Collection/4.webp'
+  },
+  { 
+    id: 4,
+    label: 'Ethnic',
+    image: '/assets/pages/homepage/sections/7-Collection/3.jpg'
+  },
+  {
+    id: 5,
+    label: 'Minimalist',
+    image: '/assets/pages/homepage/sections/7-Collection/5.jpg'
+  }
+];
+
+const collectionsNum = 5;
+const counter = collections.length / collectionsNum;
+const rowsLength = collections.length - (counter * 2);
+
+
+export default function Collections() {
+
+  const { breakpoints: { md }, screenWidth } = useWindowSize();
+
+  let pos = 0;
+  let rowNum = 1;
+
+  const isEven = value => value % 2 === 0;
+
+  return (
+    <section id="collections" className="flex flex-col justify-center items-center gap-12">
+      <h2 className="heading text-center px-5 text-4xl uppercase text-primaryFont">
+        Collections
+      </h2>
+
+      <div className={`w-full grid grid-rows-${rowsLength} gap-2 px-5 mx-auto sm:px-7 md:px-10 lg:px-12 xl:px-15 2xl:px-17`}>
+        {Array.from({ length: counter }).map((_, index) =>
+          <React.Fragment key={index}>
+            {screenWidth < md &&
+              <div className={`row-${rowNum++} collection-${pos + 1} relative`}>
+                <div className={`
+                  col-1
+                  img-cont
+                  relative
+                  w-full h-[50vw]
+                  sm:h-[40vw]
+                  md:h-[35vw]
+                  lg:h-[30vw]
+                  xl:h-[25vw]
+                  2xl:h-[20vw]
+                `}>
+                  <Image
+                    className="object-cover object-center rounded-xl"
+                    fill
+                    src={collections[pos]?.image}
+                    alt={collections[pos]?.label}
+                  />
+                </div>
+                <div className="absolute left-0 bottom-0 w-full flex flex-wrap flex-col items-start justify-center gap-1 px-3 py-3 text-xs font-semibold overlay-black-50 after:rounded-b-xl text-white sm:flex-row sm:justify-between sm:items-center sm:text-sm md:text-base">
+                  <div className="label ms-0.5 z-10 uppercase">
+                    {collections[pos++]?.label}
+                  </div>
+                  <button className="border px-2 py-1 z-10 rounded-lg">
+                    View
+                  </button>
+                </div>
+              </div>
+            }
+
+            <div className={`row-${rowNum++} grid grid-cols-10 gap-2`}>
+              <div className={`
+                collection-${pos + 1}
+                col-1 col-span-4
+                ${isEven(index) ? "order-last" : "order-first"}
+                relative
+              `}>
+                <div className={`
+                  img-cont
+                  relative
+                  w-full h-[50vw]
+                  sm:h-[40vw]
+                  md:h-[35vw]
+                  lg:h-[30vw]
+                  xl:h-[25vw]
+                  2xl:h-[20vw]
+                `}>
+                  <Image
+                    className="object-cover object-center rounded-xl"
+                    fill
+                    src={collections[pos]?.image}
+                    alt={collections[pos]?.label}
+                  />
+                </div>
+                <div className="absolute left-0 bottom-0 w-full flex flex-wrap flex-col items-start justify-center gap-1 px-3 py-3 text-xs font-semibold overlay-black-50 after:rounded-b-xl text-white sm:flex-row sm:justify-between sm:items-center sm:text-sm md:text-base">
+                  <div className="label ms-0.5 z-10 uppercase">
+                    {collections[pos++]?.label}
+                  </div>
+                  <button className="border px-2 py-1 z-10 rounded-lg">
+                    View
+                  </button>
+                </div>
+              </div>
+              <div className={`collection-${pos + 1} col-2 col-span-6 relative`}>
+                <div className={`
+                  img-cont
+                  relative
+                  w-full h-[50vw]
+                  sm:h-[40vw]
+                  md:h-[35vw]
+                  lg:h-[30vw]
+                  xl:h-[25vw]
+                  2xl:h-[20vw]
+                `}>
+                  <Image
+                    className="object-cover object-center rounded-xl"
+                    fill
+                    src={collections[pos]?.image}
+                    alt={collections[pos]?.label}
+                  />
+                </div>
+                <div className="absolute left-0 bottom-0 w-full flex flex-wrap flex-col items-start justify-center gap-1 px-3 py-3 text-xs font-semibold overlay-black-50 after:rounded-b-xl text-white sm:flex-row sm:justify-between sm:items-center sm:text-sm md:text-base">
+                  <div className="label ms-0.5 z-10 uppercase">
+                    {collections[pos++]?.label}
+                  </div>
+                  <button className="border px-2 py-1 z-10 rounded-lg">
+                    View
+                  </button>
+                </div>
+              </div>
+            </div>
+
+            <div className={`row-${rowNum++} grid grid-cols-12 gap-2 md:grid-cols-11`}>
+              <div className={`
+                collection-${pos + 1}
+                col-1 col-span-6
+                relative
+                md:col-span-3
+                ${isEven(index) ? "md:order-first" : "md:order-last"}
+              `}>
+                <div className={`
+                  img-cont
+                  relative
+                  w-full
+                  h-[50vw]
+                  sm:h-[40vw]
+                  md:h-[35vw]
+                  lg:h-[30vw]
+                  xl:h-[25vw]
+                  2xl:h-[20vw]
+                `}>
+                  <Image
+                    className="object-cover object-center rounded-xl"
+                    fill
+                    src={collections[pos]?.image}
+                    alt={collections[pos]?.label}
+                  />
+                </div>
+                <div className="absolute left-0 bottom-0 w-full flex flex-wrap flex-col items-start justify-center gap-1 px-3 py-3 text-xs font-semibold overlay-black-50 after:rounded-b-xl text-white sm:flex-row sm:justify-between sm:items-center sm:text-sm md:text-base">
+                  <div className="label ms-0.5 z-10 uppercase">
+                    {collections[pos++]?.label}
+                  </div>
+                  <button className="border px-2 py-1 z-10 rounded-lg">
+                    View
+                  </button>
+                </div>
+              </div>
+              <div className={`collection-${pos + 1} col-2 col-span-6 relative md:col-span-4`}>
+                <div className={`
+                  img-cont
+                  relative
+                  w-full h-[50vw]
+                  sm:h-[40vw]
+                  md:h-[35vw]
+                  lg:h-[30vw]
+                  xl:h-[25vw]
+                  2xl:h-[20vw]
+                `}>
+                  <Image
+                    className="object-cover object-center rounded-xl"
+                    fill
+                    src={collections[pos]?.image}
+                    alt={collections[pos]?.label}
+                  />
+                </div>
+                <div className="absolute left-0 bottom-0 w-full flex flex-wrap flex-col items-start justify-center gap-1 px-3 py-3 text-xs font-semibold overlay-black-50 after:rounded-b-xl text-white sm:flex-row sm:justify-between sm:items-center sm:text-sm md:text-base">
+                  <div className="label ms-0.5 z-10 uppercase">
+                    {collections[pos++]?.label}
+                  </div>
+                  <button className="border px-2 py-1 z-10 rounded-lg">
+                    View
+                  </button>
+                </div>
+              </div>
+
+              {screenWidth >= md &&
+                <div className={`collection-${pos + 1} col-3 relative md:col-span-4`}>
+                  <div className={`
+                    img-cont
+                    relative
+                    w-full h-[50vw]
+                    sm:h-[40vw]
+                    md:h-[35vw]
+                    lg:h-[30vw]
+                    xl:h-[25vw]
+                    2xl:h-[20vw]
+                  `}>
+                    <Image
+                      className="object-cover object-center rounded-xl"
+                      fill
+                      src={collections[pos]?.image}
+                      alt={collections[pos]?.label}
+                    />
+                  </div>
+                  <div className="absolute left-0 bottom-0 w-full flex flex-wrap flex-col items-start justify-center gap-1 px-3 py-3 text-xs font-semibold overlay-black-50 after:rounded-b-xl text-white sm:flex-row sm:justify-between sm:items-center sm:text-sm md:text-base">
+                    <div className="label ms-0.5 z-10 uppercase">
+                      {collections[pos++]?.label}
+                    </div>
+                    <button className="border px-2 py-1 z-10 rounded-lg">
+                      View
+                    </button>
+                  </div>
+                </div>
+              }
+            </div>
+          </React.Fragment>
+        )}
+      </div>
+    </section>
+  );
+}
