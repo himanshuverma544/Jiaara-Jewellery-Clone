cafff756c59de0645ddaa2b2b7b9a870386d0925
Himanshu Verma <himanshuverma544@gmail.com>
2024-09-23 10:40:26 +0530

    upd: indentation fixed

--- src/components/pages/home/key-benefits/components/Benefit.jsx
+++ src/components/pages/home/key-benefits/components/Benefit.jsx
@@ -4,7 +4,15 @@ import Icon from "@/components/general/Icon";
 export default function Benefit({ className, icon, heading, text }) {
 
   return (
-    <div className={`benefit w-full flex items-center gap-5 p-4 text-white sm:w-1/2 lg:w-fit ${className}`}>
+    <div className={`
+      benefit
+      w-full
+      flex items-center gap-5 p-4
+      text-white
+      sm:w-1/2
+      lg:w-fit
+      ${className}
+    `}>
       <Icon className="text-3xl" icon={icon}/>
       <div className="content flex flex-col gap-1">
         <div className="heading text-sm uppercase">
