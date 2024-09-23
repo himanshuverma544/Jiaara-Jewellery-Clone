d23d6893224e8075dad67cba3f5008e4d17e89c7
Himanshu Verma <himanshuverma544@gmail.com>
2024-09-23 10:46:04 +0530

    upd: updated the functionality of Icon Component

--- src/components/general/Icon.jsx
+++ src/components/general/Icon.jsx
@@ -1,9 +1,48 @@
-const Icon = ({ className = "", icon: TheIcon, style = {} }) => (
-  
-  <TheIcon 
-    className={`icon ${className}`} 
-    style={style}
-  />
-);
+import React from 'react';
+import Image from 'next/image';
+
+
+function isReactElement(icon) {
+  return React.isValidElement(icon);
+}
+
+function isReactComponent(icon) {
+  return (
+    typeof icon === 'function' &&
+    (
+      icon.prototype?.isReactComponent || 
+      String(icon).includes('createElement') || 
+      React.isValidElement(React.createElement(icon))
+    )
+  );
+}
+
+
+const Icon = ({ icon, alt, className, innerClassName, ...props }) => {
+
+  if (isReactElement(icon)) {
+    return (
+      React.cloneElement(icon, { 'className': className, 'aria-label': alt, ...props })
+    );
+  }
+  else if (isReactComponent(icon)) {
+    const TheIcon = icon;
+    return (
+      <TheIcon className={className} aria-label={alt} {...props}/>
+    );
+  }
+  else {
+    return (
+      <div className={className} {...props}>
+        <Image
+          className={innerClassName}
+          fill
+          src={icon}
+          alt={alt}
+        />
+      </div>
+    );
+  }
+};
 
 export default Icon;
