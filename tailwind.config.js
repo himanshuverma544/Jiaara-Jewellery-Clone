eb274630735589cecc58dd712ecaa666ac9a5b5a
Himanshu Verma <himanshuverma544@gmail.com>
2024-09-24 15:44:59 +0530

    upd: added new color

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,7 @@ module.exports = {
         tertiaryBackground: "#e5bae7",
         quaternaryBackground: "#b783a7",
         quinaryBackground: "#f8e9f3",
+        senaryBackground: "#e1d1dc", 
 
         primaryFont: "#663c59",
         primaryButton: "#ffddf4",
