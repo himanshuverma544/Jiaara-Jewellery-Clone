1732663e51e9f2858a6d75b4dd65ebc11a65f58a
Himanshu Verma <himanshuverma544@gmail.com>
2024-10-03 11:12:43 +0530

    add: created function to get discount percentage

---
+++ src/utils/functions/getDiscountPercentage.js
@@ -0,0 +1,6 @@
+export default function getDiscountPercentage({ actualPrice = 0, discountedPrice = 0 }) {
+
+  const percentageDiscount = Math.floor(((parseInt(actualPrice) - parseInt(discountedPrice)) * 100) / parseInt(actualPrice));
+
+  return percentageDiscount;
+}
