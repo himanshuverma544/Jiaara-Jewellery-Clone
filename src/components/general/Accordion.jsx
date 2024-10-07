15afad331dae30a91ed06860e3aed7ad81a54c48
Himanshu Verma <himanshuverma544@gmail.com>
2024-10-07 12:47:48 +0530

    add: created the accordion component

---
+++ src/components/general/Accordion.jsx
@@ -0,0 +1,67 @@
+import "@/styles/accordion-animations.css";
+
+import { useState } from 'react';
+import { CSSTransition } from 'react-transition-group';
+
+import Icon from "./Icon";
+
+const Content = Icon;
+
+
+const Accordion = ({
+  className = "",
+  title = "",
+  titleClassName = "",
+  content = <></>,
+  contentClassName = "",
+  openIcon = "+",
+  closeIcon = "-",
+  iconClassName = "",
+  timeout = 300
+}) => {
+
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleAccordion = () => {
+    setIsOpen(!isOpen);
+  };
+
+
+  return (
+    <div className={`accordion ${className} border-b`}>
+      <button
+        onClick={toggleAccordion}
+        className={`
+          toggle-button
+          w-full
+          flex justify-between items-center py-4
+          text-left
+          focus:outline-none
+        `}
+      >
+        <span className={`accordion-title ${titleClassName}`}>
+          {title}
+        </span>
+        <span className={`accordion-toggle-icon-cont ${iconClassName}`}>
+          {!isOpen ? <Icon icon={openIcon}/> : <Icon icon={closeIcon}/>}
+        </span>
+      </button>
+
+      <CSSTransition
+        in={isOpen}
+        timeout={timeout}
+        classNames="accordion"
+        unmountOnExit
+      >
+        <Content
+          className={contentClassName}
+          icon={content}
+        />
+      </CSSTransition>
+    </div>
+  );
+};
+
+
+export default Accordion;
