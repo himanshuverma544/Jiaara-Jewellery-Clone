48f00ed55b59afc3c9eeaf2d38b1bf20145f0d91
Himanshu Verma <himanshuverma544@gmail.com>
2024-10-15 13:11:18 +0530

    upd: imported and used the created sections

--- src/app/single-product/page.jsx
+++ src/app/single-product/page.jsx
@@ -1,8 +1,16 @@
+import ProductDisplay from "@/components/pages/single-product/product-display/ProductDisplay";
+import ProductsCarousel from "@/components/global/ProductsCarousel";
+import KeyBenefits from "@/components/global/key-benefits/KeyBenefits";
+import RelatedProducts from "@/components/pages/single-product/RelatedProducts";
+
 export default function SingleProduct() {
 
   return (
-    <div className="single-product-page flex flex-col gap-12">
-      
+    <div className="single-product-page flex flex-col
+     gap-12 mt-28">
+      <ProductDisplay/>
+      <KeyBenefits/>
+      <RelatedProducts/>
     </div>
   );
 }
