db29d3673b24e5688f2c95169786c474499faf73
Himanshu Verma <himanshuverma544@gmail.com>
2024-11-02 13:03:25 +0530

    upd: made the AutoSelect component more general

--- src/components/general/AutoSelect.jsx
+++ src/components/general/AutoSelect.jsx
@@ -1,5 +1,6 @@
 import { useState, useRef, useEffect } from "react";
 
+import InputField from "./InputField";
 import Icon from "./Icon";
 
 import useScrollIntoView from "@/utils/hooks/general/useScrollIntoView";
@@ -7,11 +8,21 @@ import useClickOutside from "@/utils/hooks/general/useClickOutside";
 
 
 const AutoSelect = ({
-  placeholder = "",
+  className = "",
+  inputGroupClassName = "",
+  label = {},
+  input = {},
+  validation = {},
+  dropdownClassName = "",
+  optionClassName = {
+    default: "",
+    hover: "",
+    selection: ""
+  },
   options = [],
   arrowDropdownIcon: ArrowDropdown = ({
     className = "", size = 20, stroke = "", fill = "none", onClick = () => {}
-  }) => 
+  }) =>
     <svg
       className={`arrow-drop-down-icon ${className}`}
       width={size}
@@ -27,7 +38,7 @@ const AutoSelect = ({
 }) => {
 
 
-  const [inputValue, setInputValue] = useState("");
+  const [inputValue, setInputValue] = useState(options[25] || "");
   const [filteredOptions, setFilteredOptions] = useState(options);
 
   const [isDropdownVisible, setDropdownVisible] = useState(false);
@@ -64,7 +75,7 @@ const AutoSelect = ({
   const toggleDropdown = () => {
 
     if (isDropdownVisible) {
-      inputRef.current.blur();
+      inputRef.current?.blur();
     }
     setFilteredOptions(options);
     setDropdownVisible(!isDropdownVisible);
@@ -81,50 +92,53 @@ const AutoSelect = ({
   useClickOutside(dropdownRef, () => {
 
     setDropdownVisible(false);    
-    inputRef.current.blur();      
+    inputRef.current?.blur();      
   });
 
 
   return (
-    <div ref={dropdownRef} className="relative">
-      <div
-        className="input-wrapper flex justify-between items-center rounded-e-md"
+    <div ref={dropdownRef} className={`relative ${className}`}>
+
+      <InputField
+        className={`${inputGroupClassName}`}
         onClick={toggleDropdown}
-      >
-        <input
-          ref={inputRef}
-          className={`
-            w-full border rounded-s-md p-3
-            outline-none
-            hover:ring-1 hover:ring-secondaryBackground
-            focus:ring-1 focus:ring-primaryFont
-          `}
-          type="text"
-          value={inputValue}
-          onChange={handleInputChange}
-          placeholder={placeholder}
-          autoComplete="off"
-        />
-        <Icon
-          className={isDropdownVisible && "rotate-180"}
-          icon={<ArrowDropdown/>}
-        />
-      </div>
+
+        input={{
+          ref: inputRef,
+          icon: {
+            className: `${isDropdownVisible && "rotate-180"} text-primaryFont`,
+            theIcon: <Icon icon={<ArrowDropdown/>}/>
+          },
+          value: inputValue,
+          onChange: handleInputChange,
+          ...input
+        }}
+        
+        label={{
+          ...label
+        }}
+        
+        validation={{
+          ...validation
+        }}
+      />
 
       {isDropdownVisible && filteredOptions.length > 0 && (
-        <ul className="w-full h-[11rem] absolute border mt-0.5 overflow-y-auto">
-          {filteredOptions.map((option, index) => (
+        <ul className={`w-full absolute top-[97.9%] inset-x-0 mt-0.5 z-10 select-none ${dropdownClassName}`}>
+          {filteredOptions.map((option, index) =>
             <li
               key={index}
               ref={selectedOption === option ? scrollRef : null}
-              className={`cursor-pointer px-3 py-2 hover:bg-gray-200 ${
-                selectedOption === option ? "bg-gray-300" : ""
-              }`}
+              className={`
+                cursor-pointer px-3 py-2 ${optionClassName.default}
+                ${optionClassName.hover}
+                ${selectedOption === option && optionClassName.selection}
+              `}
               onClick={() => handleSelect(option)}
             >
               {option}
             </li>
-          ))}
+          )}
         </ul>
       )}
     </div>
