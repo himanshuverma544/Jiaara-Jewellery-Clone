b4789dee14277b414c02675a9294ea318715d95d
Himanshu Verma <himanshuverma544@gmail.com>
2024-11-04 11:02:24 +0530

    add: added sidebar utils hook

---
+++ src/utils/hooks/sidebar/useSidebarUtils.jsx
@@ -0,0 +1,40 @@
+import { useState, useEffect, useRef } from 'react';
+
+
+export default function useSidebarUtils() {
+
+  const [isOpen, setIsOpen] = useState(false);
+  const innerRef = useRef(null);
+
+
+  const handleClickOutside = event => {
+    if (innerRef.current && !innerRef.current.contains(event.target)) {
+      setIsOpen(false);
+    }
+  };
+
+  const handleEscapePress = event => {
+    if (event.key === 'Escape') {
+      setIsOpen(false);
+    }
+  };
+
+  useEffect(() => {
+
+    const sidebarContNode = document.querySelector('.sidebar-cont');
+
+    if (isOpen) {
+      sidebarContNode.addEventListener('mousedown', handleClickOutside);
+      document.addEventListener('keydown', handleEscapePress);
+    }
+    return () => {
+      sidebarContNode.removeEventListener('mousedown', handleClickOutside);
+      document.removeEventListener('keydown', handleEscapePress);
+    };
+  }, [isOpen]);
+
+  return ({
+    sidebarState: [isOpen, setIsOpen],
+    innerRef
+  });
+}
