06a68be3afaedfd858cd26317fa6f2697fe0f44e
Himanshu Verma <himanshuverma544@gmail.com>
2024-11-14 10:58:45 +0530

    upd: fetched categories form api and used the data for display

--- src/components/pages/home/Categories.jsx
+++ src/components/pages/home/Categories.jsx
@@ -8,50 +8,21 @@ import {
   Slide,
 } from 'pure-react-carousel';
 
+import { useQuery } from "@tanstack/react-query";
+
 import useVisibleSlides from '@/utils/hooks/pure-react-carousel/useVisibleSlides';
 
+import { getCategories } from "@/utils/functions/api/cms/woocommerce/categories";
 
-const categories = [
-  {
-    id: 1,
-    categoryName: "Brass",
-    image: "/assets/pages/homepage/products/wearing-product/brass.webp",
-    imageAlt: "image 1",
-    totalProducts: 10
-  },
-
-  {
-    id: 2,
-    categoryName: "Ethnic",
-    image: "/assets/pages/homepage/products/wearing-product/ethnic.jpg",
-    imageAlt: "image 2",
-    totalProducts: 20
-  },
-  {
-    id: 3,
-    categoryName: "Oxidised",
-    image: "/assets/pages/homepage/products/wearing-product/necklace.jpg",
-    imageAlt: "image 3",
-    totalProducts: 30
-  },
-  {
-    id: 4,
-    categoryName: "Minimalist",
-    image: "/assets/pages/homepage/products/wearing-product/minimalist.jpg",
-    imageAlt: "image 4",
-    totalProducts: 40
-  },
-  {
-    id: 5,
-    categoryName: "Indo-Western",
-    image: "/assets/pages/homepage/products/wearing-product/indo-western.jpg",
-    imageAlt: "image 5",
-    totalProducts: 50
-  }
-];
+import skipMap from "@/utils/functions/general/skipMap";
 
 
 export default function Categories() {
+
+  const { data: parentCategories, isSuccess } = useQuery({
+    queryKey: ['parent-categories'],
+    queryFn: () => getCategories({ parent: 0 })
+  });
 
   const { visibleSlidesCount } = useVisibleSlides({
     desktopVisibleSlidesCount: 3,
@@ -65,51 +36,53 @@ export default function Categories() {
       <h2 className="heading text-center text-4xl uppercase text-primaryFont">
         Shop by Category
       </h2>
-
-      <CarouselProvider
-        className="carousel w-screen px-3"
-        naturalSlideWidth={100}
-        naturalSlideHeight={125}
-        isIntrinsicHeight
-        visibleSlides={visibleSlidesCount}
-        totalSlides={categories.length}
-        isPlaying
-        interval={3000}
-      >
-        <Slider className="categories-slide select-none cursor-grab active:cursor-grabbing">
-          {categories.map((category, index) =>
-            <Slide
-              key={category.id}
-              index={index}
-              className="mx-[2.5vw]"
-              innerClassName="relative"
-            >
-              <div className="img-cont relative w-full h-[80vw] sm:h-[40vw] lg:h-[25vw]">
-                <Image
-                  className="object-cover object-center rounded-3xl"
-                  fill
-                  src={category.image}
-                  alt={category.imageAlt}
-                />
-              </div>
-              
-              <div className="content absolute bottom-0 w-full flex justify-between uppercase p-5 overlay-black-50 after:rounded-b-3xl text-white">
-                <div className="category flex flex-col gap-2 z-10">
-                  <div className="category-text text-sm font-semibold">
-                    {category.categoryName}
-                  </div>
-                  <div className="total-products text-xs opacity-50">
-                    {`${category.totalProducts} Products`}
-                  </div>
+    
+      {(isSuccess && parentCategories.length > 0) &&
+        <CarouselProvider
+          className="carousel w-screen px-3"
+          naturalSlideWidth={100}
+          naturalSlideHeight={125}
+          isIntrinsicHeight
+          visibleSlides={visibleSlidesCount}
+          totalSlides={parentCategories.length}
+          isPlaying
+          interval={3000}
+        >
+          <Slider className="categories-slide select-none cursor-grab active:cursor-grabbing">
+            {skipMap(parentCategories, [{ name: "General" }], (parentCategory, index) =>
+              <Slide
+                key={parentCategory?.id}
+                index={index}
+                className="mx-[2.5vw]"
+                innerClassName="relative"
+              >
+                <div className="img-cont relative w-full h-[80vw] sm:h-[40vw] lg:h-[25vw]">
+                  <Image
+                    className="object-cover object-center rounded-3xl"
+                    fill
+                    src={parentCategory?.image}
+                    alt={parentCategory?.slug}
+                  />
                 </div>
-                <button className="z-10 border px-3 py-1 rounded-xl">
-                  View All
-                </button>
-              </div>
-            </Slide>
-          )}
-        </Slider>   
-      </CarouselProvider>
+                
+                <div className="content absolute bottom-0 w-full flex justify-between uppercase p-5 overlay-black-50 after:rounded-b-3xl text-white">
+                  <div className="category flex flex-col gap-2 z-10">
+                    <div className="category-text text-sm font-semibold">
+                      {parentCategory?.name}
+                    </div>
+                    <div className="total-products text-xs opacity-50">
+                      {`${parentCategory?.totalProducts} Products`}
+                    </div>
+                  </div>
+                  <button className="z-10 border px-3 py-1 rounded-xl">
+                    View All
+                  </button>
+                </div>
+              </Slide>
+            )}
+          </Slider>   
+        </CarouselProvider>
+      }
     </section>
   );
 }
