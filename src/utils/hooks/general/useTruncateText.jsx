b347e761340bcb477a6f570984a550e0a0cd8872
Himanshu Verma <himanshuverma544@gmail.com>
2024-11-21 11:08:44 +0530

    upd: updated the functionality of the hook to work with function as well

--- src/utils/hooks/general/useTruncateText.jsx
+++ src/utils/hooks/general/useTruncateText.jsx
@@ -1,23 +1,38 @@
-import { useState } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 
 
-function useTruncateText({ text = "", wordLimit = 10 }) {
+function useTruncateText({ text = "", wordLimit = 10 } = {}) {
 
+  const [truncatedText, setTruncatedText] = useState('');
   const [isExpanded, setIsExpanded] = useState(false);
 
-  const toggleText = () => {
-    setIsExpanded(!isExpanded);
+  const createTruncateText = (text, wordLimit) => {
+    return (
+      text.split(' ').length > wordLimit ?
+        text.split(' ').slice(0, wordLimit).join(' ') + '…' : text
+    );
   };
 
-  const truncatedText = text.split(' ').length > wordLimit
-    ? text.split(' ').slice(0, wordLimit).join(' ') + '…'
-    : text;
+  const getTruncateText = useCallback((text, wordLimit) => {
+    return createTruncateText(text, wordLimit);
+  }, []);
+
+  useEffect(() => {
+    if (text && wordLimit) {
+      const truncatedText = createTruncateText(text, wordLimit);
+      setTruncatedText(truncatedText);
+    }
+  }, [text, wordLimit]);
+
+  const toggleText = () => setIsExpanded(!isExpanded);
 
   return {
     isExpanded,
     toggleText,
-    displayText: isExpanded ? text : truncatedText
+    displayText: isExpanded ? text : truncatedText,
+    getTruncateText
   };
 }
+
 
 export default useTruncateText;
