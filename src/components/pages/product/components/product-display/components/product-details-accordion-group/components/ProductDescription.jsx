87da709961874e3ee3da08110a12504c686d10aa
Himanshu Verma <himanshuverma544@gmail.com>
2024-11-21 11:30:40 +0530

    add: created a product description component

---
+++ src/components/pages/product/components/product-display/components/product-details-accordion-group/components/ProductDescription.jsx
@@ -0,0 +1,15 @@
+import Icon from "@/components/general/Icon";
+
+const Content = Icon;
+
+export default function ProductDescription({ className = "", product = null }) {
+
+  return (
+    <div className={`description ${className} px-[5vw] pb-5 md:px-[4vw]`}>
+      <Content
+        className="flex flex-col gap-5 bullet-all text-xs text-gray-800 xl:text-sm"
+        icon={product?.description}
+      />
+    </div>
+  );
+}
