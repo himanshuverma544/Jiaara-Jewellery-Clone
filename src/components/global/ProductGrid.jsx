38cd88c37250472c0451140c8f2b8aeeef5742aa
Himanshu Verma <himanshuverma544@gmail.com>
2024-11-21 11:42:36 +0530

    upd: updated the styles of the product grid

--- src/components/global/ProductGrid.jsx
+++ src/components/global/ProductGrid.jsx
@@ -1,57 +1,10 @@
 import Product from "./Product";
 
-
-const products = [
-  {
-    id: 1,
-    image: "/assets/pages/homepage/products/only-product/1.webp",
-    name: "Pearlime Jewellery",
-    price: "9,200.00",
-  },
-  {
-    id: 2,
-    image: "/assets/pages/homepage/products/only-product/2.webp",
-    name: "Tambina Jewellery",
-    price: "6,300.00",
-  },
-  {
-    id: 3,
-    image: "/assets/pages/homepage/products/only-product/3.webp",
-    name: "Sambina Jewellery",
-    price: "7,500.00",
-  },
-  {
-    id: 4,
-    image: "/assets/pages/homepage/sections/3-FeaturedProducts/1.webp",
-    name: "Amalita Earrings Pearl",
-    price: "5,200.00",
-  },
-  {
-    id: 5,
-    image: "/assets/pages/homepage/sections/3-FeaturedProducts/2.webp",
-    name: "Bambina Earrings White",
-    price: "25,027.99",
-  },
-  {
-    id: 6,
-    image: "/assets/pages/homepage/sections/3-FeaturedProducts/3.webp",
-    name: "Bambina Earrings Green",
-    price: "25,027.99",
-  },
-  {
-    id: 7,
-    image: "/assets/pages/homepage/sections/3-FeaturedProducts/4.webp",
-    name: "Amalita Earrings Green",
-    price: "24,457.99",
-  }
-];
-
-
-export default function ProductGrid({ className = "" }) {
+export default function ProductGrid({ className = "", products = [] }) {
 
   return (
-    <div className={`product-grid flex flex-wrap justify-center items-center gap-5 p-5 ${className}`}>
-      {products.map(product => 
+    <div className={`product-grid flex flex-wrap justify-center items-center gap-x-[5vw] gap-y-10 p-[5vw] ${className}`}>
+      {products.map(product =>
         <Product
           key={product.id}
           className="w-[35vw] max-w-[9rem]"
