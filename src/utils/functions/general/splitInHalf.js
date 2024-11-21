c6a456280c7fe725cfbe1af44008cbd92a5a20f4
Himanshu Verma <himanshuverma544@gmail.com>
2024-11-21 11:10:43 +0530

    add: created the function to spit it into half

---
+++ src/utils/functions/general/splitInHalf.js
@@ -0,0 +1,11 @@
+export default function splitInHalf(input) {
+
+  const isArray = Array.isArray(input);
+  const entries = isArray ? input : Object?.entries(input);
+
+  const mid = Math.ceil(entries.length / 2);
+
+  const splitResult = [entries.slice(0, mid), entries.slice(mid)];
+
+  return isArray ? splitResult : splitResult.map(part => Object.fromEntries(part));
+};
