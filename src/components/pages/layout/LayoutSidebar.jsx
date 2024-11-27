58e98333b2a5ab96f09b8e6e5dce7b1bc68c37fd
Himanshu Verma <himanshuverma544@gmail.com>
2024-11-27 18:44:25 +0530

    add: created the sidebar which opens up on clicking hamburger icon

---
+++ src/components/pages/layout/LayoutSidebar.jsx
@@ -0,0 +1,156 @@
+'use client';
+
+import { useEffect, useContext } from "react";
+import { context } from "@/context-API/context";
+import { storeData } from "@/context-API/actions/action.creators";
+
+import Sidebar from "@/components/general/Sidebar";
+import Accordion from "@/components/general/Accordion";
+
+import { useQuery } from "@tanstack/react-query";
+
+import { MdOutlineKeyboardArrowUp, MdOutlineKeyboardArrowDown } from "react-icons/md";
+import { AiOutlineLoading3Quarters } from "react-icons/ai";
+
+import useSidebarUtils from "@/utils/hooks/sidebar/useSidebarUtils";
+
+import { CATEGORIES, COLLECTIONS } from "@/routes";
+import { getCategories } from "@/utils/functions/api/cms/woocommerce/categories";
+import { getCollections } from "@/utils/functions/api/cms/woocommerce/collections";
+
+import skipMap from "@/utils/functions/general/skipMap";
+
+
+export default function LayoutSidebar() {
+
+  const { dispatch } = useContext(context);
+
+  const { sidebarState: [isOpen, setIsOpen], innerRef } = useSidebarUtils();
+
+
+  useEffect(() => {
+
+    function storeComponentData() {
+      dispatch(storeData({ layoutSidebar: [isOpen, setIsOpen] }, "states"));
+    }
+    storeComponentData();
+
+  }, [isOpen, setIsOpen, dispatch]);
+
+
+  const {
+    data: parentCategories,
+    isLoading: isParentCategoriesLoading,
+    isSuccess: isParentCategoriesSuccess
+  } =
+  useQuery({
+    queryKey: ['parent-categories'],
+    queryFn: () => getCategories({ parent: 0 }),
+  });
+
+
+  const {
+    data: collections,
+    isLoading: isCollectionsLoading,
+    isSuccess: isCollectionsSuccess
+  } = 
+  useQuery({
+    queryKey: ['general-collections'],
+    queryFn: getCollections,
+  });
+
+
+  const Categories = ({ parentCategories, isSuccess }) => (
+    isSuccess && (
+      <ul className="categories flex flex-col gap-5 ps-5 py-5">
+        {skipMap(parentCategories, [{ name: "General" }], (parentCategory, index) => (
+          <li key={parentCategory?.id || index} className="flex justify-between items-center">
+            <div className="category-name">
+              {parentCategory?.name}
+            </div>
+            <div className="products-count">
+              {`(${parentCategory?.count})`}
+            </div>
+          </li>
+        ))}
+      </ul>
+    )
+  );
+
+  const Collections = ({ collections, isSuccess }) => (
+    isSuccess && (
+      <ul className="collections flex flex-col gap-5 ps-5 py-5">
+        {collections.map((collection, index) => (
+          <li key={collection?.id || index} className="flex justify-between items-center">
+            <div className="collection-name">
+              {collection?.name}
+            </div>
+            <div className="products-count">
+              {`(${collection?.count})`}
+            </div>
+          </li>
+        ))}
+      </ul>
+    )
+  );
+
+
+  return (
+    <Sidebar
+      innerRef={innerRef}
+      isOpen={isOpen}
+      innerClassName="bg-white w-64"
+    >
+      <ul className="sidebar-navigations flex flex-col justify-center px-5">
+        <li className="categories-item">
+          <Accordion
+            className="border-primaryFont text-sm"
+            title={CATEGORIES?.title}
+            content={
+              <Categories
+                parentCategories={parentCategories}
+                isSuccess={isParentCategoriesSuccess}
+              />
+            }
+            iconClassName={isParentCategoriesLoading ? "animate-spin" : "text-xl"}
+            openIcon={
+              isParentCategoriesLoading ? AiOutlineLoading3Quarters : MdOutlineKeyboardArrowDown
+            }
+            closeIcon={
+              isCollectionsLoading ? AiOutlineLoading3Quarters : MdOutlineKeyboardArrowUp
+            }
+            divider={{
+              upper: { isEnabled: true },
+              bottom: { isEnabled: true },
+            }}
+          />
+        </li>
+
+        <li className="collections-item">
+          <Accordion
+            className="border-primaryFont text-sm"
+            title={COLLECTIONS?.title}
+            contentClassName="content text-xs"
+            content={
+              <Collections
+                collections={collections}
+                isSuccess={isCollectionsSuccess}
+              />
+            }
+            iconClassName={isCollectionsLoading ? "animate-spin" : "text-xl"}
+            openIcon={
+              isCollectionsLoading ? AiOutlineLoading3Quarters : MdOutlineKeyboardArrowDown
+            }
+            closeIcon={
+              isCollectionsLoading ? AiOutlineLoading3Quarters : MdOutlineKeyboardArrowUp
+            }
+            divider={{
+              upper: { isEnabled: true },
+              bottom: { isEnabled: false },
+            }}
+          />
+        </li>
+      </ul>
+    </Sidebar>
+  );
+}
