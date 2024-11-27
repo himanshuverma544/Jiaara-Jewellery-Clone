7aec1c24f8eb71b530cc289436755aab226bc2a2
Himanshu Verma <himanshuverma544@gmail.com>
2024-11-27 18:59:44 +0530

    upd: increased the functionality - added hamburger - created enableNavItem and disableNavItem functions to hide routes as per requirement

--- src/components/pages/layout/Header.jsx
+++ src/components/pages/layout/Header.jsx
@@ -8,12 +8,16 @@ import Link from 'next/link';
 
 import { getAllRoutes } from '@/routes';
 
+import AutoSelect from '@/components/general/AutoSelect';
+import HamburgerMenu from '@/components/general/HamburgerMenu';
+
 import NavItem from '@/components/general/NavItem';
 
-import useLinkActive from '@/utils/hooks/general/useLinkActive';
+import useWindowSize from '@/utils/hooks/general/useWindowSize';
+import useRouteActive from '@/utils/hooks/general/useRouteActive';
 
 
-const { HOME, SHOP, CART } = getAllRoutes();
+const { HOME, SHOP, CATEGORIES, COLLECTIONS, CART, WISHLIST, SEARCH } = getAllRoutes();
 
 const brandLogo = {
   white: "/assets/logos/jiaara-white.png",
@@ -23,11 +27,34 @@ const brandLogo = {
 
 export default function Header() {
 
-  const { isActive: isHomepage } = useLinkActive({ href: HOME?.pathname });
+  const { screenWidth, breakpoints: { md, lg } } = useWindowSize();
 
+
+  const { isActive: isHomepage } = useRouteActive({ href: HOME?.pathname });
+  
   const { data: { triggered } = {}, data: { states } = {} } = useContext(context) || {};
-
+  
   const isHeroSecVisible = isHomepage && (triggered && states?.isHeroSecVisible);
+
+
+  const disableNavItem = (currentRoute, routes, breakpoint = md) => {
+
+    if (!currentRoute || !currentRoute.id || !Array.isArray(routes)) return false;
+    return !(routes.some(route => route.id === currentRoute.id) && screenWidth >= breakpoint);
+  };
+  
+  const enableNavItem = (currentRoute, routes, breakpoint = md) => {
+
+    if (!currentRoute || !currentRoute.id || !Array.isArray(routes)) return true;
+    return !(routes.some(route => route.id === currentRoute.id) && screenWidth < breakpoint);
+  };
+
+
+  const [isOpen, setIsOpen]
+    = triggered && Array.isArray(states?.layoutSidebar) ? states?.layoutSidebar : [false, () => {}];
+
+  const toggleSidebar = () => setIsOpen(!isOpen);
+  
 
   return (
     <header
@@ -43,9 +70,10 @@ export default function Header() {
         <div className="img-cont size-[100px] relative">
           <Image
             fill
-            src={isHeroSecVisible ? brandLogo.white : brandLogo.black}
+            src={isHeroSecVisible ? brandLogo?.white : brandLogo?.black}
             alt="brand-logo"
             sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+            priority
           />
         </div>
       </Link>
@@ -59,14 +87,15 @@ export default function Header() {
             bg-secondaryBackground
             lg:justify-evenly lg:px-0 lg:py-5 lg:uppercase lg:bg-transparent
           `}>
-            {[HOME, SHOP].map(route => 
+            {[HOME, SEARCH, SHOP].map(route => 
               <NavItem
                 key={route?.id}
                 title={route?.title}
                 href={route?.pathname}
                 icon={{
                   active: route?.activeIcon,
-                  inactive: route?.inactiveIcon
+                  inactive: route?.inactiveIcon,
+                  general: route?.generalIcon
                 }}
                 linkClass={`
                   flex flex-col items-center justify-center gap-2 px-3 py-3
@@ -86,33 +115,45 @@ export default function Header() {
                   text-primaryFont
                   ${isHeroSecVisible ? "lg:text-white" : "lg:text-primaryFont"}
                 `}
+                enabled={disableNavItem(route, [SEARCH], lg)}
               />
             )}
+
           </ul>
         </nav>
 
-        <div className="nav-icons-cont grow flex flex-col items-end justify-center py-8 lg:items-baseline">
-          <div className={`
-            flex gap-4 px-3
+        <nav className="nav-icons-cont grow flex flex-col items-end justify-center py-8 lg:items-baseline">
+          <ul className={`
+            flex justify-center items-center gap-4 px-3
             text-lg
             ${isHeroSecVisible ? "text-white" : "text-primaryFont"}
           `}>
-            <NavItem
-              href={CART?.pathname}
-              icon={{
-                active: CART?.activeIcon,
-                inactive: CART?.inactiveIcon
-              }}
+            {[SEARCH, WISHLIST, CART].map(route =>
+              <NavItem
+                key={route?.id}
+                href={route?.pathname}
+                icon={{
+                  active: route?.activeIcon,
+                  inactive: route?.inactiveIcon,
+                  general: route?.generalIcon
+                }}
+                enabled={enableNavItem(route, [SEARCH], lg)}
+              />
+            )}
+            <HamburgerMenu
+              innerClassName={`w-4 h-px my-1 ${isHeroSecVisible ? "bg-white" : "bg-primaryFont"}`}
+              isOpen={isOpen}
+              onClick={toggleSidebar}
             />
-          </div>
+          </ul>
 
           <hr className={`
             design-line
             w-full absolute top-[93%] right-0
             ${isHeroSecVisible ? "border-white" : "border-primaryFont"}
           `}/>
-        </div>
-        
+        </nav>
+
       </div>
     </header>
   );
