1bfba6c0eb26c03ebfa5704821a6c905b3ecd34b
Himanshu Verma <himanshuverma544@gmail.com>
2024-11-28 13:05:38 +0530

    upd: made the NavItemDropdown worked

--- src/components/pages/layout/Header.jsx
+++ src/components/pages/layout/Header.jsx
@@ -1,20 +1,29 @@
 'use client';
 
-import { useContext } from 'react';
-import { context } from "@/context-API/context";
-
 import Image from 'next/image';
 import Link from 'next/link';
 
-import { getAllRoutes } from '@/routes';
+import { useContext } from 'react';
+import { context } from "@/context-API/context";
 
-import AutoSelect from '@/components/general/AutoSelect';
-import HamburgerMenu from '@/components/general/HamburgerMenu';
+import { useQuery } from "@tanstack/react-query";
 
 import NavItem from '@/components/general/NavItem';
+import NavItemDropdown from '@/components/general/AutoSelect';
+import HamburgerMenu from '@/components/general/HamburgerMenu';
+
+import { MdOutlineArrowDropDown } from "react-icons/md";
+import { AiOutlineLoading3Quarters } from "react-icons/ai";
 
 import useWindowSize from '@/utils/hooks/general/useWindowSize';
 import useRouteActive from '@/utils/hooks/general/useRouteActive';
+
+import { getCategories } from "@/utils/functions/api/cms/woocommerce/categories";
+import { getCollections } from "@/utils/functions/api/cms/woocommerce/collections";
+
+import skipMap from "@/utils/functions/general/skipMap";
+
+import { getAllRoutes } from '@/routes';
 
 
 const { HOME, SHOP, CATEGORIES, COLLECTIONS, CART, WISHLIST, SEARCH } = getAllRoutes();
@@ -37,6 +46,12 @@ export default function Header() {
   const isHeroSecVisible = isHomepage && (triggered && states?.isHeroSecVisible);
 
 
+  const [isOpen, setIsOpen]
+    = triggered && Array.isArray(states?.layoutSidebar) ? states?.layoutSidebar : [false, () => {}];
+
+  const toggleSidebar = () => setIsOpen(!isOpen);
+
+
   const disableNavItem = (currentRoute, routes, breakpoint = md) => {
 
     if (!currentRoute || !currentRoute.id || !Array.isArray(routes)) return false;
@@ -49,12 +64,55 @@ export default function Header() {
     return !(routes.some(route => route.id === currentRoute.id) && screenWidth < breakpoint);
   };
 
-
-  const [isOpen, setIsOpen]
-    = triggered && Array.isArray(states?.layoutSidebar) ? states?.layoutSidebar : [false, () => {}];
-
-  const toggleSidebar = () => setIsOpen(!isOpen);
   
+  const requiredCategories = categories => {
+
+    return skipMap(categories, [{ name: "General" }], category =>
+      ({ 
+        id: category?.id,
+        name: category?.name,
+        slug: category?.slug,
+        count: category?.count,
+        url: `/category/${category?.id}`
+      })
+    );
+  }
+
+  const requiredCollections = collections => {
+
+    return collections.map(collection =>
+      ({ 
+        id: collection?.id,
+        name: collection?.name,
+        slug: collection?.slug,
+        count: collection?.count,
+        url: `/collection/${collection?.id}`
+      })
+    );
+  }
+
+
+  const {
+    data: parentCategories,
+    isLoading: isParentCategoriesLoading,
+    isSuccess: isParentCategoriesSuccess
+  } =
+  useQuery({
+    queryKey: ['parent-categories'],
+    queryFn: () => getCategories({ parent: 0 }),
+  });
+
+
+  const {
+    data: collections,
+    isLoading: isCollectionsLoading,
+    isSuccess: isCollectionsSuccess
+  } = 
+  useQuery({
+    queryKey: ['general-collections'],
+    queryFn: getCollections,
+  });
+
 
   return (
     <header
@@ -111,7 +169,7 @@ export default function Header() {
                 `}
                 titleClass={`
                   nav-item-title
-                  text-xs
+                  text-sm
                   text-primaryFont
                   ${isHeroSecVisible ? "lg:text-white" : "lg:text-primaryFont"}
                 `}
@@ -119,6 +177,100 @@ export default function Header() {
               />
             )}
 
+            {screenWidth > lg &&
+              <li className="categories-nav-item nav-item list-none">
+                <NavItemDropdown
+                  className="categories-dropdown text-sm"
+                  inputGroupClassName="cursor-default"
+                  isLinkMode={true}
+                  input={{
+                    id: "categories-dropdown",
+                    className: `
+                      w-[8rem] pt-1 pb-2 uppercase bg-transparent
+                      hover:ring-transparent focus:ring-transparent cursor-default 
+                      ${isHeroSecVisible ? "text-white" : "text-primaryFont"}
+                    `,
+                    defaultValue: CATEGORIES?.title,
+                    icon: {
+                      className: `
+                        mb-1 text-lg
+                        ${isHeroSecVisible ? "text-white" : "text-primaryFont"}
+                        ${isParentCategoriesLoading ? "text-xs animate-spin" : ""}
+                      `,
+                      theIcon:
+                        isParentCategoriesLoading ?
+                          <AiOutlineLoading3Quarters/> : <MdOutlineArrowDropDown/>
+                    },
+                    autoComplete: "off",
+                    readOnly: true
+                  }}
+                  dropdownClassName={`
+                    w-[13rem] rounded-sm
+                    ${isHeroSecVisible ?
+                      "backdrop-blur bg-opacity-20 bg-white text-white" :
+                      "bg-secondaryBackground text-primaryFont"
+                    }   
+                  `}
+                  options={isParentCategoriesSuccess ? requiredCategories(parentCategories) : []}
+                  optionClassName={{
+                    hover: `
+                      ${isHeroSecVisible ?
+                        "hover:backdrop-blur hover:bg-opacity-[1%] hover:bg-white hover:text-white" :
+                        "hover:bg-primaryFont hover:text-white"
+                      }
+                    `,
+                    link: "flex justify-between items-center"
+                  }}
+                />
+              </li>
+            }
+
+            {screenWidth >= lg &&
+              <li className="collections-nav-item nav-item list-none">
+                <NavItemDropdown
+                  className="collections-dropdown text-sm"
+                  isLinkMode={true}
+                  input={{
+                    id: "collections-dropdown",
+                    className: `
+                      w-[9rem] pt-1 pb-2 uppercase bg-transparent
+                      hover:ring-transparent focus:ring-transparent cursor-default 
+                      ${isHeroSecVisible ? "text-white" : "text-primaryFont"}
+                    `,
+                    defaultValue: COLLECTIONS?.title,
+                    icon: {
+                      className: `
+                        mb-1 text-lg
+                        ${isHeroSecVisible ? "text-white" : "text-primaryFont"}
+                        ${isCollectionsLoading ? "text-xs animate-spin" : ""}
+                      `,
+                      theIcon:
+                        isCollectionsLoading ?
+                          <AiOutlineLoading3Quarters/> : <MdOutlineArrowDropDown/>
+                    },
+                    autoComplete: "off",
+                    readOnly: true
+                  }}
+                  dropdownClassName={`
+                    w-[13rem] rounded-sm
+                    ${isHeroSecVisible ?
+                      "backdrop-blur bg-opacity-20 bg-white text-white" :
+                      "bg-secondaryBackground text-primaryFont"
+                    }   
+                  `}
+                  options={isCollectionsSuccess ? requiredCollections(collections) : []}
+                  optionClassName={{
+                    hover: `
+                      ${isHeroSecVisible ?
+                        "hover:backdrop-blur hover:bg-opacity-[1%] hover:bg-white hover:text-white" :
+                        "hover:bg-primaryFont hover:text-white"
+                      }
+                    `,
+                    link: "flex flex-wrap justify-between items-center"
+                  }}
+                />
+              </li>
+            }
           </ul>
         </nav>
 
@@ -140,11 +292,13 @@ export default function Header() {
                 enabled={enableNavItem(route, [SEARCH], lg)}
               />
             )}
-            <HamburgerMenu
-              innerClassName={`w-4 h-px my-1 ${isHeroSecVisible ? "bg-white" : "bg-primaryFont"}`}
-              isOpen={isOpen}
-              onClick={toggleSidebar}
-            />
+            {screenWidth < lg &&
+              <HamburgerMenu
+                innerClassName={`w-4 h-px my-1 ${isHeroSecVisible ? "bg-white" : "bg-primaryFont"}`}
+                isOpen={isOpen}
+                onClick={toggleSidebar}
+              />
+            }
           </ul>
 
           <hr className={`
