8b1ad649676d5af61a740d678e143d11156480ca
Himanshu Verma <himanshuverma544@gmail.com>
2024-12-01 03:15:31 +0530

    upd: used the contact-links component

--- src/components/pages/layout/Footer.jsx
+++ src/components/pages/layout/Footer.jsx
@@ -1,12 +1,7 @@
 import Link from "next/link";
 import Image from "next/image";
 
-import { RxInstagramLogo } from "react-icons/rx";
-import { RiFacebookBoxLine } from "react-icons/ri";
-import { MdMailOutline, MdOutlineLocalPhone } from "react-icons/md";
-import { GrLocation } from "react-icons/gr";
-
-import Icon from "@/components/general/Icon";
+import ContactLinks from "@/components/global/ContactLinks";
 
 import {
   HOME,
@@ -16,50 +11,6 @@ import {
   TERMS_AND_CONDITIONS,
   RETURN_REFUND_CANCELLATION_POLICY
 } from "@/routes";
-
-
-const contactLinks = [
-  {
-    id: "instagram",
-    className: "instagram-handler-link",
-    icon: <RxInstagramLogo/>,
-    name: "instagram.com/jiaarajewellery",
-    url: "https://www.instagram.com/jiaarajewellery/",
-    target: "_blank"
-  },
-  {
-    id: "facebook",
-    className: "facebook-handler-link",
-    icon: <RiFacebookBoxLine/>,
-    name: "facebook.com/Jiaara-100068747121541",
-    url: "https://www.facebook.com/Jiaara-100068747121541/",
-    target: "_blank"
-  },
-  {
-    id: "mail",
-    className: "mail-address-link",
-    icon: <MdMailOutline/>,
-    name: "jiaaracreations@gmail.com",
-    url: "mailto:jiaaracreations@gmail.com",
-    target: "_blank"
-  },
-  {
-    id: "contact-num",
-    className: "contact-number-link",
-    icon: <MdOutlineLocalPhone/>,
-    name: "+91 639 687 2895",
-    url: "tel:+916396872895",
-    target: "_self"
-  },
-  {
-    id: "location",
-    className: "location-address-link",
-    icon: <GrLocation/>,
-    name: "1003, Bldg-6, Sandstone Society, Unique Garden, Kanakia Layout, Mira Road, Thane-East, Bhayander, 401107",
-    url: "#location.",
-    target: "_self"
-  }
-];
 
 
 const helpAndPolicesLink = [
@@ -107,31 +58,13 @@ export default function Footer() {
         bg-tertiaryBackground
       `}
     >
-
       <div className="wrapper w-full flex flex-col gap-10 sm:flex-row sm:justify-between sm:gap-16 md:gap-0 md:justify-evenly">
 
         <div className="connect-with-us-cont flex flex-col gap-5 justify-center sm:w-1/2 sm:justify-start">
           <div className="heading font-semibold">
             Stay connected with us
           </div>
-          <div className="links-cont flex flex-col gap-5 justify-center">
-            {contactLinks.map((contactLink, index) =>
-              <Link
-                key={contactLink?.id || index}
-                className={`${contactLink?.className} w-fit flex gap-3 hover:font-semibold`}
-                href={contactLink?.url}
-                target={contactLink?.target}
-                scroll={false}
-              >
-                <div className="icon-cont">
-                  <Icon className="icon text-xl" icon={contactLink?.icon}/>
-                </div>
-                <div className="name text-xs xs:text-sm">
-                  {contactLink?.name}
-                </div>
-              </Link>
-            )}
-          </div>
+          <ContactLinks/>
         </div>
 
         <div className="customer-care-cont flex flex-col gap-5 justify-center sm:w-1/2 sm:justify-start md:w-auto">
