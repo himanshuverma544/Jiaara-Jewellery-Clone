f1885529cf45aee3eeb7378e04653632a71bee62
Himanshu Verma <himanshuverma544@gmail.com>
2024-12-01 13:07:44 +0530

    upd: extended it's functionality to accept simple arguments, as well as object

--- src/utils/functions/general/isFalsy.js
+++ src/utils/functions/general/isFalsy.js
@@ -1,8 +1,10 @@
-export default function isFalsy({ value = 0, exclude = [] }) {
+export default function isFalsy(args = {}, value = 0, exclude = []) {
 
   const falsyValues = [0, -0, false, '', undefined, null, NaN];
 
-  const theFalsyValues = falsyValues.filter(falsyValue => !exclude.includes(falsyValue));
+  const theFalsyValues = falsyValues.filter(
+    falsyValue => !args?.exclude?.includes(falsyValue) || !exclude?.includes(falsyValue)
+  );
 
-  return theFalsyValues.includes(value);
+  return theFalsyValues.includes(args?.value || value);
 }
