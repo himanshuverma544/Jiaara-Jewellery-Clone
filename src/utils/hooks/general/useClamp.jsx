8e4b97e87348f5ff933060ee022d09ba541b9391
Himanshu Verma <himanshuverma544@gmail.com>
2024-12-02 11:09:09 +0530

    rm: removed the comments

--- src/utils/hooks/general/useClamp.jsx
+++ src/utils/hooks/general/useClamp.jsx
@@ -16,24 +16,22 @@ const { screenWidth, breakpoints } = useWindowSize();
       { breakpoint: Infinity, values: xxl }
     ];
   
-    // Find the correct values for the current screen width, falling back to the closest available if necessary
-    let selectedSetting = xs; // Default to the smallest size
+  
+    let selectedSetting = xs;
   
     for (let i = 0; i < settings.length; i++) {
 
       const { breakpoint, values } = settings[i];
 
       if (screenWidth < breakpoint) {
-        // Find the first defined size for the current breakpoint or the closest previous one
         selectedSetting = values.min !== undefined ? values : selectedSetting;
         break;
       }
       else if (values.min !== undefined) {
-        // If the screenWidth is larger, keep updating with the available defined values
         selectedSetting = values;
       }
     }
-    // Return the calculated font size string
+
     return (
       `min(max(
         ${selectedSetting.min}rem,
