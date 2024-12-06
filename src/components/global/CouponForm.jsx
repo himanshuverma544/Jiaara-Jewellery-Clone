b6005a2e60ba39560c49337f2e52f5bdd99e1a1a
Himanshu Verma <himanshuverma544@gmail.com>
2024-12-06 17:41:19 +0530

    upd: wrote functionality for coupon

--- src/components/global/CouponForm.jsx
+++ src/components/global/CouponForm.jsx
@@ -1,5 +1,11 @@
 'use client';
 
+import { useContext } from 'react';
+import { context } from "@/context-API/context";
+import { storeData } from "@/context-API/actions/action.creators";
+
+import { useForm } from 'react-hook-form';
+
 import { PiTagThin } from "react-icons/pi";
 
 import InputField from "@/components/general/InputField";
@@ -7,15 +13,25 @@ import InputField from "@/components/general/InputField";
 
 export default function CouponForm({ className = "" }) {
 
+  const { dispatch } = useContext(context);
+  
+  const { control, handleSubmit } = useForm({ mode: "onChange" });
+
+  const onApplyCoupon = data => {
+
+    dispatch(storeData({ couponData: data }, "objects"));
+  }
+
   return (
     <form
       className={`coupon-form flex items-end gap-5 ${className}`}
-      onSubmit={event => event.preventDefault()}
+      onSubmit={handleSubmit(onApplyCoupon)}
     >
       <InputField
         className="w-full"
         input={{
           id: "coupon-code",
+          inputName: "couponCode",
           className: "w-[inherit] ps-3 pe-10 py-3 rounded-md text-sm input-selection-primaryFont focus:ring-primaryFont hover:ring-secondaryBackground",
           placeholder: "e.g., SAVE50",
           icon: {
@@ -27,10 +43,11 @@ export default function CouponForm({ className = "" }) {
           className: "text-sm text-primaryFont",
           text: "Coupon Code"
         }}
+        control={control}
       />
       <button
         type="submit"
-        className="coupon-code-apply-btn rounded-md px-[7vw] py-3 text-sm text-white bg-primaryFont"
+        className="coupon-code-apply-btn rounded-md px-[7vw] py-3 text-sm uppercase text-white bg-primaryFont"
       >
         Apply
       </button>
