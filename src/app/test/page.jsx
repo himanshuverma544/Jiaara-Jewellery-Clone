1b6d8b8d5a5ce7250a1fd4ee256d09ee84e41bdf
Himanshu Verma <himanshuverma544@gmail.com>
2024-12-25 15:32:29 +0530

    add: created test page and try component for trying and testing things

---
+++ src/app/test/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Try from '@/components/test/Try';
+
+
+export default function Test() {
+
+  return (
+    <div className="test">
+      <Try/>
+    </div>
+  );
+}
