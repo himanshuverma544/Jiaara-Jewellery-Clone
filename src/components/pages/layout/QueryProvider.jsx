dde81a6c3e348a53f40007e12ba4cdfb869561fa
Himanshu Verma <himanshuverma544@gmail.com>
2024-12-28 19:24:46 +0530

    upd: persisted the react query data in localStorage

--- src/components/pages/layout/QueryProvider.jsx
+++ src/components/pages/layout/QueryProvider.jsx
@@ -1,21 +1,43 @@
-'use client';
+import { useEffect, useMemo } from "react";
 
-import { useState } from 'react';
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { persistQueryClient } from "@tanstack/react-query-persist-client";
+import { createSyncStoragePersister } from "@tanstack/query-sync-storage-persister";
 
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { compress, decompress } from "lz-string";
 
 
 export default function QueryProvider({ children }) {
 
-  const params = {
+  const queryClient = useMemo(() => new QueryClient({
     defaultOptions: {
       queries: {
-        staleTime: Infinity
+        staleTime: Infinity,
+        cacheTime: 1000 * 60 * 60 * 24,
+        gcTime: 1000 * 60 * 60 * 24
       }
     }
-  };
+  }), []);
 
-  const [queryClient] = useState(() => new QueryClient(params));
+
+  useEffect(() => {
+    
+    if (typeof window !== "undefined") {
+
+      const localStoragePersister = createSyncStoragePersister({
+        storage: window.localStorage,
+        serialize: data => compress(JSON.stringify(data)),
+        deserialize: data => JSON.parse(decompress(data))
+      });
+
+      persistQueryClient({
+        queryClient,
+        persister: localStoragePersister,
+        maxAge: 1000 * 60 * 60 * 24
+      });
+    }
+  }, [queryClient]);
+
 
   return (
     <QueryClientProvider client={queryClient}>
