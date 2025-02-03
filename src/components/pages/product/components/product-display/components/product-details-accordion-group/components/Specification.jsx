d87a6ace9201216797d53b8a9ff473001d13a52f
Himanshu Verma <himanshuverma544@gmail.com>
2025-02-03 15:55:18 +0530

    upd: normalize the white space between attributes

--- src/components/pages/product/components/product-display/components/product-details-accordion-group/components/Specification.jsx
+++ src/components/pages/product/components/product-display/components/product-details-accordion-group/components/Specification.jsx
@@ -9,17 +9,24 @@ export default function Specification({ className = "", product = null }) {
 
   const [leftSpecs, rightSpecs] = splitInHalf(product?.specifications);
 
-  const getBorderClassName = (index) => {
+
+  const getBorderClassName = index => {
 
     if (index && (index !== 0 & index !== 1 || index === 1 && screenWidth < md)) {
       return "border-t border-gray-300";
     }
   }
 
+  const normalizeWhitespace = text => {
+
+    return text.replace(/[\W_]+/g, " ").trim();
+  }
+
+
   return (
     <div className={`specification pb-3 overflow-x-auto text-xs xl:text-sm ${className}`}>
       <table className="min-w-full table-auto border-collapse">
-        <tbody className="md:grid md:grid-cols-2 md:gap-x-10"> 
+        <tbody className="capitalize md:grid md:grid-cols-2 md:gap-x-10"> 
 
           {Object.entries(leftSpecs).map(([specificationName, specificationValue], index) => (
             <tr
@@ -27,10 +34,10 @@ export default function Specification({ className = "", product = null }) {
               className={`${getBorderClassName(index)} md:flex md:justify-between`}
             >
               <td className="w-1/2 px-4 py-2 font-medium text-gray-600">
-                {specificationName}
+                {normalizeWhitespace(specificationName)}
               </td>
               <td className="w-1/2 px-4 py-2 text-gray-800">
-                {specificationValue}
+                {normalizeWhitespace(specificationValue)}
               </td>
             </tr>
           ))}
@@ -41,10 +48,10 @@ export default function Specification({ className = "", product = null }) {
               className={`border-t border-gray-300 md:flex md:justify-between`}
             >
               <td className="w-1/2 px-4 py-2 text-gray-600 font-medium">
-                {specificationName}
+                {normalizeWhitespace(specificationName)}
               </td>
               <td className="w-1/2 px-4 py-2 text-gray-800">
-                {specificationValue}
+                {normalizeWhitespace(specificationValue)}
               </td>
             </tr>
           ))}
