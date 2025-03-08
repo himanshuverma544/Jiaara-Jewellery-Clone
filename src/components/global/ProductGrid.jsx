a718afb51ed11fa3a254094c66a180e5858480e7
Himanshu Verma <himanshuverma544@gmail.com>
2025-03-08 20:23:23 +0530

    upd: made the grid adaptable and responsive as per the sidebar

--- src/components/global/ProductGrid.jsx
+++ src/components/global/ProductGrid.jsx
@@ -1,6 +1,7 @@
 'use client';
 
-import { memo } from "react";
+import { useContext, memo } from "react";
+import { context } from "@/context-API/context";
 
 import Product from "@/components/global/Product";
 
@@ -10,13 +11,16 @@ const ProductGrid = ({
   products = null
 }) => {
   
+  const { data: { triggered } = {}, data: { states } = {} } = useContext(context) || {};
+  const [isSidebarOpen] = triggered ? states?.sidebar || [] : [];
+
   return (
     <div
       className={`
         product-grid
-        flex flex-wrap justify-between items-center gap-x-[5vw] gap-y-10 px-5 py-5
+        flex flex-wrap justify-between items-center gap-x-1 gap-y-10 px-5 py-5
         xs:px-[5vw]
-        sm:justify-start sm:px-[4vw]
+        sm:justify-start sm:gap-x-[3.2vw] md:gap-x-[3vw] sm:px-[4vw] md:px-[3vw]
         ${className}
       `}
     >
@@ -26,17 +30,18 @@ const ProductGrid = ({
           className={`
             w-[87vw]
             2xs:w-[42vw]
-            sm:w-[27vw]
-            lg:w-[18vw]
-            2xl:w-[14vw]
+            sm:w-[28.5vw]
+            md:w-[29.3vw]
+            lg:w-[20.9vw]
+            ${isSidebarOpen ? "lg:w-[29.8vw] xl:w-[20.6vw]" : "lg:w-[20.9vw]"}
           `}
           imgContClassName={`
             w-full relative
             h-[87vw]
             2xs:h-[42vw]
-            sm:h-[27vw]
-            lg:h-[18vw]
-            2xl:h-[14vw]
+            sm:h-[28.5vw]
+            md:h-[29.3vw]
+            ${isSidebarOpen ? "lg:h-[29.8vw] xl:h-[20.6vw]" : "lg:h-[20.9vw]"}
           `}
           productDetailsContClassName="text-xs text-primaryFont"
           productNameClassName="uppercase"
